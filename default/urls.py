3e49faad1476199e690c0a0c09434e986a4d8192
arisu0316 <kathy40133@yahoo.com.tw>
2019-04-10 16:11:12 +0800

    檢視投票主題

--- default/urls.py
+++ default/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .veiws import *
 
 urlpatterns = [
-    path('poll/', poll_list),
-    path('poll1/', PollList.as_view()),
+    #path('poll/', poll_list),
+    path('poll/', PollList.as_view()),
+    path('poll/<int:pk>/', PollDetail.as_view()),
 ]
