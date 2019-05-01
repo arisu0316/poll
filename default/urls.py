aa55bbed23bbf34f90ef4e92cc619d9174fb93c0
arisu0316 <kathy40133@yahoo.com.tw>
2019-05-01 15:13:49 +0800

    完成投票選項修改,刪除

--- default/urls.py
+++ default/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path('poll/', PollList.as_view()),
     path('poll/<int:pk>/', PollDetail.as_view()),
     path('vote/<int:oid>/', PollVote.as_view()),
+    path('poll/create/', PollCreate.as_view()),
+    path('poll/<int:pk>/update/',PollCreate.as_view()),
+    path('poll/<int:pk>/delete/',PollDelete.as_view()),
+    path('option/create/<int:pid>/',OptionCreate.as_view()),
+    path('option/<int:pk>/update/',OptionCreate.as_view()),
+    path('option/<int:pk>/delete/',OptionDelete.as_view()),
 ]
--- default/views.py
+++ default/views.py
@@ -26,3 +26,45 @@ class PollVote(RedirectView):
         return '/poll/{}/'.format(item.poll_id)
         # return '/poll/'+str(item.poll_id)+'/' 也可以用
 
+class PollCreate(CreateView):
+    model = Poll
+    fields = ['subject']
+    success_url = '/poll/'
+    #template_name = 
+
+class PollUpdate(UpdateView):
+    model = Poll
+    fields = ['subject']
+    success_url = "/poll/"
+    
+class PollDelete(DeleteView):
+    model = Poll
+    success_url = "/poll/"
+    template_name = 'comfirm_delete.html'
+
+class OptionCreate(CreateView):
+    model = Option
+    fields = ['title']
+    template_name = 'default/poll_form.html'
+
+    def get_success_url(self):
+        return '/poll/{}/'.format(self.kwargs['pid'])
+    
+    def form_valid(self, form):
+        form.instance.poll_id = self.kwargs['pid']
+        return super().form_valid(form)
+
+  class OptionUpdate(UpdateView):
+    model = Option
+    fields = ['title']  
+    template_name = 'default/poll_form.html'  
+
+    def get_success_url(self):
+        return '/poll/{}/'.format(self.object.poll_id)
+
+class OptionDelete(DeleteView):
+    model = Option
+    template_name = 'confirm_dalete.html'
+
+    def get_success_url(self):
+        return '/poll/{}/'.format(self.kwargs['pid'])
